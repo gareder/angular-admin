2746bb60bf30aaf825ebf88f4a3f5930eaeadac1
Eder Garcia <ederjogarcia@gmail.com>
2020-09-30 16:50:01 -0500

    settings service

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SettingsService } from '../services/settings.service';
 
 @Component({
   selector: 'app-pages',
@@ -7,12 +8,9 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class PagesComponent implements OnInit {
-  public linkTheme = document.querySelector('#theme');
-  constructor() { }
 
-  ngOnInit(): void {
-    const url = localStorage.getItem('theme') || './assets/css/colors/default-dark.css';
-    this.linkTheme.setAttribute('href', url);
-  }
+  constructor(private settingService: SettingsService) { }
+
+  ngOnInit(): void { }
 
 }
