38c1b11cfa9cedac67d1f4f1ccd47f351e49d012
Eder Garcia <ederjogarcia@gmail.com>
2020-10-15 14:43:53 -0500

    profile, image upload

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import Swal from 'sweetalert2';
 import { User } from 'src/app/models/user.model';
 import { UserService } from '../../services/user.service';
+import { FileUploadService } from '../../services/file-upload.service';
 
 @Component({
   selector: 'app-profile',
@@ -13,8 +14,10 @@ export class ProfileComponent implements OnInit {
 
   public profileForm: FormGroup;
   public user: User;
+  public uploadFile: File;
+  public tempImg: any = null;
 
-  constructor(private fb: FormBuilder, private userService: UserService) {
+  constructor(private fb: FormBuilder, private userService: UserService, private fileUploadService: FileUploadService) {
     this.user = userService.user;
   }
 
@@ -32,8 +35,45 @@ export class ProfileComponent implements OnInit {
       this.user.email = email;
       Swal.fire({
         title: 'Success',
-        text: 'Profile update successfully',
+        text: 'Profile updated successfully',
         icon: 'success'
+      });
+    }, (err) => {
+      Swal.fire({
+        title: 'Error',
+        text: err.error.msg,
+        icon: 'error'
+      });
+    });
+  }
+
+  changeImage(file: File) {
+    this.uploadFile = file;
+    if (!file) {
+      // If there's no image, we set the temp to null to make sure it won't show on the view
+      return this.tempImg = null;
+    }
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      this.tempImg = reader.result;
+    }
+  }
+
+  uploadImage() {
+    this.fileUploadService.uploadImage(this.uploadFile, 'users', this.user.id).then(img => {
+      this.user.img = img;
+      Swal.fire({
+        title: 'Success',
+        text: 'Profile image updated successfully',
+        icon: 'success'
+      }).catch(err => {
+        console.log(err);
+        Swal.fire({
+          title: 'Error',
+          text: 'Error at trying to upload the image',
+          icon: 'error'
+        });
       });
     });
   }
---
+++ src/app/services/file-upload.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+const base_url = environment.base_url;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FileUploadService {
+
+  constructor() { }
+
+  async uploadImage(file: File, type: 'users'|'medics'|'hospitals', id: string) {
+
+    try {
+
+      const url = `${ base_url }/uploads/${ type }/${ id }`;
+      const formData = new FormData();
+      formData.append('image', file);
+      const resp = await fetch(url, {
+        method: 'PUT',
+        headers: {
+          'x-token': localStorage.getItem('token') || ''
+        },
+        body: formData
+      });
+      const data = await resp.json();
+      if (data.ok) {
+        return data.imageName;
+      } else {
+        console.log(data.msg);
+        return false;
+      }
+
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+}
